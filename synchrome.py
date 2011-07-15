ca2864331552aec8d2fbf483bea2d73b006e4e68
Clément Mondon <clement.mondon@gmail.com>
2011-07-15 11:18:01 +0200

    Distinct Main & Synchromizers files

--- synchrome.py
+++ synchrome.py
@@ -2,102 +2,8 @@
 # -*- coding: UTF-8 -*-
 # Copyright 2011 Amandine Degand, Clément Mondon
 
-import os
-import hashlib 
-import pickle
 import argparse
-from sys import stdout
-
-dir_index = ".synchrome/"
-file_index = ".synchrome"
-
-class Synchromizer():
-    """ Synchomizer entitie """
-    def __init__(self, name, path='.'):
-        self.name = name
-        self.path = path
-        self.filelist = {}
-        self.synchromelist = {}
-        self.modified = {}
-        try:
-            f = open(self.path+os.sep+dir_index+os.sep+file_index, "r")
-            self.synchromelist = pickle.load(f) 
-            print "File found"
-            f.close()
-        except (IOError , EOFError):
-            print "File not found or empty"
-            try:
-                f = open(self.path+os.sep+dir_index+os.sep+file_index, "w")
-                pickle.dump({}, f) 
-                f.close()
-            except (IOError , EOFError):
-                print "FatalError"
-
-    def add(self,filepath):
-        "Add a file to synchromizer"
-        self.synchromelist[filepath] = [md5sum(filepath),]
-
-    def save(self):
-        "Write filelist file"
-        f = open(self.path+os.sep+dir_index+os.sep+file_index, "w")
-        pickle.dump(self.synchromelist.update(self.filelist), f) 
-        #pickle.dump(self.filelist, f) 
-        f.close()
-
-    def test_local_changes(self):
-        "Update modified filelist"
-        stdout.write("Verifying local changes")
-        stdout.write("Verifying local changes")
-        stdout.flush()
-        for filename, hashlist in self.filelist.items():
-
-            stdout.write(" "*30)
-            stdout.flush()
-            stdout.write("\rVerifying %s" % filename)
-            md5 = md5sum(self.path+os.sep+filename) 
-            if md5 == hashlist[0]:
-                stdout.write(" OK")
-            else:
-                stdout.write(" KO")
-                self.modified[filename] = [md5] + hashlist[:10]
-                del self.filelist[filename]
-
-        stdout.write("\rVerifying finished")
-        stdout.write(" "*30+"\n")
-        stdout.flush()
-
-    def read(self, filepath):
-        try:
-            f = open(self.path+os.sep+os.sep+filepath, "r")
-            content = f.read()
-            f.close()
-            return content
-        except (IOError , EOFError):
-            print "Error, can't write file %s" % filepath
-            return None
-
-
-    def update(self, filepath, content):
-        try:
-            f = open(self.path+os.sep+os.sep+filepath, "w")
-            f.write(content)
-            f.close()
-        except (IOError , EOFError):
-            print "Error, can't write file %s" % filepath
-
-    def fct_copy(self, filename, path1, path2):
-        import subprocess 
-        subprocess.call(["cp", path1+os.sep+filename, path2+os.sep])
-        return None
-
-    def isMe(self):
-        "return true if it is me that runs synchrome execution "
-
-    def isAvailable(self):
-        "return true if i am synchromizable "
-
-    def __str__(self):
-        return self.name + "\nManaged : " + str(self.synchromelist.keys()) + "\nUnchanged : "+ str(self.filelist.keys()) + "\n" + "Modified : " + str(self.modified.keys())
+from synchromizers import *
 
 
 class Action():
@@ -118,16 +24,6 @@ class Action():
     def __str__(self):
         return self.filename + " : [" + self.sync1.name + "] -> [" + self.sync2.name + "]"
 
-
-def md5sum(path):
-    try:
-        f=open(path,'r')
-        md5 = hashlib.md5(f.read()).hexdigest()
-        f.close()
-        return md5
-    except:
-        print "! Md5sum of %s failed" % path
-        return None
 
 def build_filelist(sync1,sync2):
     for filename, hashlist in sync1.synchromelist.iteritems():
@@ -252,21 +148,37 @@ def synchro(synchromizer1, synchromizer2):
         action.run(synchromizer1.filelist, synchromizer2.filelist)
 
 
+def add(synchromizers):
+    print "SALUT"
+
+
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Synchronization of Synchromizers -> «Synchromization»')
-    parser.add_argument('synchromizers', metavar='N', type=str, nargs='*',
+    subparsers = parser.add_subparsers(help='commands')
+
+    add_parser = subparsers.add_parser('add')
+    add_parser.add_argument('synchromizers', metavar='synchromizers', type=str, nargs='+',
                    help='one or more sychromizer to deal with')
-    parser.add_argument('--add', dest='addfile', action='store', default=None, 
-                   help='Add file to synchromizer')
-    parser.add_argument('--get', dest='addfile', action='store', default=None, 
-                   help='Get file from a synchromizer')
-    parser.add_argument('--list', dest='addfile', action='store_true', default=False, 
-                   help='List file of synchromizer')
+
+    #parser.add_argument('synchromizers', metavar='N', type=str, nargs='*',
+                   #help='one or more sychromizer to deal with')
+    #parser.add_argument('--add', dest='addfile', action='store', default=None, 
+                   #help='Add file to synchromizer')
+    #parser.add_argument('--get', dest='addfile', action='store', default=None, 
+                   #help='Get file from a synchromizer')
+    #parser.add_argument('--list', dest='addfile', action='store_true', default=False, 
+                   #help='List file of synchromizer')
 
     args = parser.parse_args()
-    print args.synchromizers
-    print args.addfile
+    arguments = dict(args._get_kwargs())
+    print arguments
+
+    #print args.synchromizers
+    #print args.addfile
+    print args
+    print add_parser
 
     actionlist = [] 
     conflicts = {}
---
+++ synchromizers.py
@@ -0,0 +1,121 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2011 Amandine Degand, Clément Mondon
+
+import os
+import pickle
+import hashlib 
+from sys import stdout
+
+dir_index = ".synchrome/"
+file_index = ".synchrome"
+
+class Synchromizer():
+    """ Synchomizer entitie """
+
+    def __init__(self, name, path='.'):
+        "Build Synchromizer"
+        self.name = name
+        self.path = path
+        self.filelist = {}
+        self.synchromelist = {}
+        self.modified = {}
+        try:
+            print "Reading file ...",
+            f = open(self.path+os.sep+dir_index+os.sep+file_index, "r")
+            try : 
+                self.synchromelist = pickle.load(f) 
+                print " [ Done ]"
+            finally:
+                f.close()
+        except (IOError , EOFError):
+            print " [ Failed ! ]"
+            #If error, attempting to create file
+            response = raw_input("Do you want to init ? (y/N)")
+            if response in ["y", "Y"]: 
+                try:
+                    f = open(self.path+os.sep+dir_index+os.sep+file_index, "w")
+                    try:
+                        pickle.dump({}, f) 
+                    finally:
+                        f.close()
+                except (IOError , EOFError):
+                    print "FatalError: Unable to create synchrome file"
+                    exit(os.EX_IOERR)
+
+    def add(self,filepath):
+        "Add a file to synchromizer"
+        self.synchromelist[filepath] = [md5sum(filepath),]
+
+    def save(self):
+        "Write filelist file"
+        f = open(self.path+os.sep+dir_index+os.sep+file_index, "w")
+        pickle.dump(self.synchromelist.update(self.filelist), f) 
+        #pickle.dump(self.filelist, f) 
+        f.close()
+
+    def test_local_changes(self):
+        "Update modified filelist"
+        stdout.write("Verifying local changes")
+        stdout.write("Verifying local changes")
+        stdout.flush()
+        for filename, hashlist in self.filelist.items():
+
+            stdout.write(" "*30)
+            stdout.flush()
+            stdout.write("\rVerifying %s" % filename)
+            md5 = md5sum(self.path+os.sep+filename) 
+            if md5 == hashlist[0]:
+                stdout.write(" OK")
+            else:
+                stdout.write(" KO")
+                self.modified[filename] = [md5] + hashlist[:10]
+                del self.filelist[filename]
+
+        stdout.write("\rVerifying finished")
+        stdout.write(" "*30+"\n")
+        stdout.flush()
+
+    def read(self, filepath):
+        try:
+            f = open(self.path+os.sep+os.sep+filepath, "r")
+            content = f.read()
+            f.close()
+            return content
+        except (IOError , EOFError):
+            print "Error, can't write file %s" % filepath
+            return None
+
+
+    def update(self, filepath, content):
+        try:
+            f = open(self.path+os.sep+os.sep+filepath, "w")
+            f.write(content)
+            f.close()
+        except (IOError , EOFError):
+            print "Error, can't write file %s" % filepath
+
+    def fct_copy(self, filename, path1, path2):
+        import subprocess 
+        subprocess.call(["cp", path1+os.sep+filename, path2+os.sep])
+        return None
+
+    def isMe(self):
+        "return true if it is me that runs synchrome execution "
+
+    def isAvailable(self):
+        "return true if i am synchromizable "
+
+    def __str__(self):
+        return self.name + "\nManaged : " + str(self.synchromelist.keys()) + "\nUnchanged : "+ str(self.filelist.keys()) + "\n" + "Modified : " + str(self.modified.keys())
+
+
+def md5sum(path):
+    try:
+        f=open(path,'r')
+        md5 = hashlib.md5(f.read()).hexdigest()
+        f.close()
+        return md5
+    except:
+        print "! Md5sum of %s failed" % path
+        return None
+
